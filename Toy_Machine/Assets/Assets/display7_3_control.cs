fbe6dde081dab2a5b03b2fa15ea0330738d6f201
BillKung <billkung.happy@gmail.com>
2018-01-12 16:36:03 +0800

    to make 7-display

---
+++ Toy_Machine/Assets/Assets/display7_3_control.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class display7_3_control : MonoBehaviour {
+	// Use this for initialization
+	public GameObject[] bars = new GameObject[10];
+	void getchild (){
+		print ("test test1");
+		bars = this.gameObject.transform.GetComponentsInChildren<GameObject>();
+	}
+	void Start () {
+		print ("test test");
+		getchild ();
+		print ("hi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
