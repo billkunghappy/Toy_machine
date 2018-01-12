814ae2fc9ded2a4323aeb9dabf826f994dcac5c7
BillKung <billkung.happy@gmail.com>
2018-01-12 23:34:58 +0800

    things now backup

---
+++ Toy_Machine/Assets/displat_3/display7_3_control.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class display7_3_control : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			gameObject.GetComponentInChildren<display_73_A>().light_on("Cube(A)",1);
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			gameObject.GetComponentInChildren<display_73_A>().light_on("Cube(A)",0);
+		}
+			
+	}
+}
